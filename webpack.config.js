daa4486be1b44fc029d71305aaeb9de489a07397
Nekomaru <Nekomaru@pku.edu.cn>
2022-05-24 22:17:03 +0800

    Add TS/React/Webpack Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/web/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'index.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                include: path.resolve(__dirname, 'src/web/'),
+                test: /\.tsx?$/,
+                use: [
+                    { loader: 'ts-loader', options: { transpileOnly: true } }
+                ]
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    externals: {
+        'react': 'React',
+        'react-dom' : 'ReactDOM',
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+    }
+};
